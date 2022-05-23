6f2d78fdadeb7bf0326a33860374dbe99171c389
Stephan van der Plas <stephan@vdplas.net>
2022-05-23 10:31:27 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    Magneet_Aan = true
+})
+input.onButtonPressed(Button.B, function () {
+    Magneet_Aan = false
+})
+let Magneet_Aan = false
+Magneet_Aan = true
 basic.forever(function () {
-	
+    if (Magneet_Aan) {
+        basic.showIcon(IconNames.Yes)
+        pins.analogWritePin(AnalogPin.P0, 1023)
+    } else {
+        basic.showIcon(IconNames.No)
+        pins.analogWritePin(AnalogPin.P0, 0)
+    }
 })
