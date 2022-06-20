eccafe44f538182fb77dbb06153962b24670f2b3
Stephan van der Plas <stephan@vdplas.net>
2022-06-20 13:18:50 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -7,8 +7,33 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.showIcon(IconNames.Yes)
     timer = 0
+    stok = lijst.pop()
+    basic.showNumber(stok)
+    if (stok == 1) {
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+    } else if (stok == 2) {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+    } else if (stok == 3) {
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    } else {
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    control.reset()
+})
+let stok = 0
 let tijd = 0
+let willekeurig = 0
+let lijst: number[] = []
 let timer = 0
 timer = 0
 basic.showIcon(IconNames.Yes)
@@ -18,7 +43,9 @@ let _1234 = [
 3,
 4
 ]
-for (let index = 0; index <= _1234.length; index++) {
-    let lijst: number[] = []
-    lijst[index] = _1234._pickRandom()
+lijst = []
+for (let index = 0; index < 4; index++) {
+    willekeurig = _1234._pickRandom()
+    lijst.push(willekeurig)
+    _1234.removeAt(willekeurig)
 }
