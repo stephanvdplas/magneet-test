b0f618f51501b22bed7943eb0e412dc8794b6803
Stephan van der Plas <stephan@vdplas.net>
2022-06-20 10:43:13 +0200

    random ingebouwd

--- main.ts
+++ main.ts
@@ -1,17 +1,24 @@
 input.onButtonPressed(Button.A, function () {
-    Magneet_Aan = true
-})
-input.onButtonPressed(Button.B, function () {
-    Magneet_Aan = false
-})
-let Magneet_Aan = false
-Magneet_Aan = true
-basic.forever(function () {
-    if (Magneet_Aan) {
-        basic.showIcon(IconNames.Yes)
-        pins.analogWritePin(AnalogPin.P0, 1023)
-    } else {
-        basic.showIcon(IconNames.No)
-        pins.analogWritePin(AnalogPin.P0, 0)
+    tijd = randint(0, 25)
+    while (timer < tijd) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+        timer += 1
     }
+    basic.showIcon(IconNames.Yes)
+    timer = 0
 })
+let tijd = 0
+let timer = 0
+timer = 0
+basic.showIcon(IconNames.Yes)
+let _1234 = [
+1,
+2,
+3,
+4
+]
+for (let index = 0; index <= _1234.length; index++) {
+    let lijst: number[] = []
+    lijst[index] = _1234._pickRandom()
+}
